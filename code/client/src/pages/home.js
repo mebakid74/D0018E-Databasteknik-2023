8c4e02e2c776812f661dea2cc9baffed164edac6
mebakid74 <mebakid74@gmail.com>
2023-02-19 16:58:27 +0100

    Homepage fixed

--- code/client/src/pages/home.js
+++ code/client/src/pages/home.js
@@ -54,20 +54,17 @@ return (
         </div>
 
         <div className="home-1">
-            <p1>LEATHER SHARP 2023 </p1> <br />
-            <p2>$28.00 USD</p2>
-            <br />
-            <h1>NEW COLLECTION</h1>
-            <p3>
-                Elevate your style with our metallic finished bovine leather accessory.
-                Expertly crafted in Spain, it features a metal buckle closure and a leather
-                belt loop for added convenience.
-            </p3>
+            <h1>LEATHER SHARP 2023 </h1>
+            <p>$28.00 USD</p>
+
+            <h2>NEW COLLECTION</h2>
+            <p2>Elevate your style with our metallic finished bovine leather accessory. </p2> <br/>
+            <p3>Expertly crafted in Spain, it features a metal buckle closure and a leather </p3> <br/>
+            <p4>belt loop for added convenience. </p4>
+
             <br />
             <button>ADD TO CART ></button>
         </div>
-
-        <br/>
 
         <div className="home-2">
             <h2>OVERSIZE CIRCLES BELT</h2>
@@ -80,38 +77,44 @@ return (
             <button>VIEW ALL ></button>
         </div>
 
-        <br/>
-
         <div className="home-3">
-            <h3>SPRING SUMMER 2023 COLLECTION</h3>
+            <h3>SPRING SUMMER <br/>
+                2023 COLLECTION</h3>
             <button>VIEW COLLECTION ></button>
         </div>
 
-        <br/>
-
         <div className="home-4">
             <h4>SEASONAL HIGHLIGHTS</h4>
-            <div className="home.prod-1">
+            <div className="homeprod-w">
+            <div className="homeprod-1">
                 <Link to="/product">
                     <img src=""/>
                     <h1>Off-white Dreamy</h1>
                 </Link>
                 <p>$95.00 USD</p>
             </div>
-            <br/>
-            <div className="home.prod-2">
+
+            <div className="homeprod-2">
                 <Link to="/product">
                     <img src=""/>
                     <h1>Off-white classics </h1>
                 </Link>
                 <p>$110.00 USD</p>
             </div>
-            <br/>
-            <div>
+
+            <div className="homeprod-3">
+                <Link to="/product">
+                    <img src=""/>
+                    <h1>Off-white limited edition </h1>
+                </Link>
+                <p>$180.00 USD</p>
+            </div>
+            </div>
+            <div className="homeprod">
                 <button>VIEW ALL ></button>
             </div>
         </div>
-        <br/>
+
         <div className="home-5">
             <h5>COLLECTION V5 COMING UP SOON</h5>
             <video loop autoPlay>
@@ -119,20 +122,25 @@ return (
             </video>
         </div>
 
-        <br/>
         <div className="home-6">
             <h6>NEWSLETTER</h6>
             <div className="cover">
                 <img src="https://images.unsplash.com/photo-1601597676886-2761d9b330bd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"/>
             </div>
-            <h7>SUBSCRIBE TO BE NOTIFIED <br/>
-                OF ALL LATEST UPDATES</h7>
-            <br/>
-            <button>SUBSCRIBE</button>
+            <div className="newsle">
+                <h1>SUBSCRIBE TO BE NOTIFIED <br/>
+                    OF ALL LATEST UPDATES</h1>
+            </div>
+            <input
+                type="text"
+                placeholder="Enter your email here"
+            />
+            <button>SUBSCRIBE ></button>
         </div>
-        <br/>
+
         <div className="home-7">
             <h8>EVERYTHING'S ATTIRE FOOTER</h8>
+            <div className="footer.w">
             <div className="footer-1">
                 <h1>Navigate</h1>
                 <ul>SHOP</ul>
@@ -156,14 +164,13 @@ return (
                 <ul>Youtube</ul>
             </div>
         </div>
+        </div>
         <br/>
         <div className="home-8">
             <h9>All rights received - 2023 EVERYTHING'S ATTIRE  </h9>
-            <br/>
             <h10>Terms & Conditions</h10>
-            <br/>
             <h11>Luleå, Sweden</h11>
-
+            <h12>2023-02-26</h12>
         </div>
 
     </div>
